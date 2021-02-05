e3e1064e7a14cd8512ffaf1d5a1fe89c85dbbd2b
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-02-05 18:08:30 -0500

    modified saif app.js

--- RestaurantApp/static/js/app saifcopy.js
+++ RestaurantApp/static/js/app saifcopy.js
@@ -1,87 +1,80 @@
-var margin = {
-  top: 20,
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
   right: 30,
   bottom: 30,
-  left: 40
-},
-width = 960 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
+  left: 30
+};
 
-// scale to ordinal because x axis is not numerical
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-//scale to numerical value by height
-var y = d3.scale.linear().range([height, 0]);
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#restaurant-plot")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
 
-var chart = d3.select("#chart")
-.append("svg") //append svg element inside #chart
-.attr("width", width + (2 * margin.left) + margin.right) //set width
-.attr("height", height + margin.top + margin.bottom); //set height
-var xAxis = d3.svg.axis()
-.scale(x)
-.orient("bottom"); //orient bottom because x-axis will appear below the bars
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-var yAxis = d3.svg.axis()
-.scale(y)
-.orient("left");
-
+// Load data from num_restaurants_ca-of-tv-watched.csv
 var url = '/api/restaurant/annex'
-d3.json(url, function(error, data) {
-x.domain(data.map(function(d) {
-  return d.category
-}));
-y.domain([0, d3.max(data, function(d) {
-  return d.num_restaurants_ca
-})]);
+d3.json(url).then(function(response) {
 
-var bar = chart.selectAll("g")
-  .data(data)
-  .enter()
-  .append("g")
-  .attr("transform", function(d, i) {
-    return "translate(" + x(d.category) + ", 0)";
-  });
+  console.log(response);
 
-bar.append("rect")
-  .attr("y", function(d) {
-    return y(d.num_restaurants_ca);
-  })
-  .attr("x", function(d, i) {
-    return x.rangeBand() + (margin.left / 2);
-  })
-  .attr("height", function(d) {
-    return height - y(d.num_restaurants_ca);
-  })
-  .attr("width", x.rangeBand()); //set width base on range on ordinal data
-
-bar.append("text")
-  .attr("x", x.rangeBand() + margin.left)
-  .attr("y", function(d) {
-    return y(d.num_restaurants_ca) - 10;
-  })
-  .attr("dy", ".75em")
-  .text(function(d) {
-    return d.num_restaurants_ca;
-  });
-
-chart.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(" + margin.left + "," + height + ")")
-  .call(xAxis);
-
-chart.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(" + margin.left + ",0)")
-  .call(yAxis)
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 6)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("num_restaurants_ca");
+// Cast the num_restaurants_ca value to a number for each piece of response
+response.forEach(function(d) {
+  d.num_restaurants_ca = +d.num_restaurants_ca;
 });
 
-function type(d) {
-d.receive_date = +d.category; // coerce to number
-return d;
-}
+// Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+var xBandScale = d3.scaleBand()
+  .domain(response.map(d => d.category))
+  .range([0, chartWidth])
+  .padding(0.1);
+
+// Create a linear scale for the vertical axis.
+var yLinearScale = d3.scaleLinear()
+  .domain([0, d3.max(response, d => d.num_restaurants_ca)])
+  .range([chartHeight, 0]);
+
+// Create two new functions passing our scales in as arguments
+// These will be used to create the chart's axes
+var bottomAxis = d3.axisBottom(xBandScale);
+var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+// Append two SVG group elements to the chartGroup area,
+// and create the bottom and left axes inside of them
+chartGroup.append("g")
+  .call(leftAxis);
+
+chartGroup.append("g")
+  .attr("transform", `translate(0, ${chartHeight})`)
+  .call(bottomAxis);
+
+// Create one SVG rectangle per piece of response
+// Use the linear and band scales to position each rectangle within the chart
+chartGroup.selectAll(".bar")
+  .data(response)
+  .enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x", d => xBandScale(d.category))
+  .attr("y", d => yLinearScale(d.num_restaurants_ca))
+  .attr("width", xBandScale.bandwidth())
+  .attr("height", d => chartHeight - yLinearScale(d.num_restaurants_ca));
+
+}).catch(function(error) {
+console.log(error);
+
+});
+
+  
