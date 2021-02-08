6dd147c20cf4514a68f72a4b06d5b6b7cf503ebf
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2021-02-08 15:26:02 -0500

    scatter, restaurant

--- RestaurantApp/static/js/scatter.js
+++ RestaurantApp/static/js/scatter.js
@@ -1,25 +1,25 @@
-var svgWidth = 700;
-var svgHeight = 500;
+var svgWidthScatter = 800;
+var svgHeightScatter = 650;
 
-var margin = {
+var marginScatter = {
     top: 20,
     right: 40,
-    bottom: 100,
-    left: 100
+    bottom: 80,
+    left: 50
   };
   
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+var widthScatter = svgWidthScatter - marginScatter.left - marginScatter.right;
+var heightScatter = svgHeightScatter - marginScatter.top - marginScatter.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("#scatter")
+var svgScatter = d3.select("#scatter")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  .attr("width", svgWidthScatter)
+  .attr("height", svgHeightScatter);
 
 // Append on SVG group
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var chartGroupScatter = svgScatter.append("g")
+  .attr("transform", `translate(${marginScatter.left}, ${marginScatter.top})`);
 
 // Initial Params
 var chosenXAxis = "average_income"
@@ -28,15 +28,13 @@ var chosenXAxis = "average_income"
 function xScale(data, chosenXAxis) {
     // create scales
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+        .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
         d3.max(data, d => d[chosenXAxis]) * 1.2
-      ])
-      .range([0, width]);
+        ])
+        .range([0, widthScatter]);
   
     return xLinearScale;
-  
-  }
-
+}
 
 // function used for updating xAxis var upon click on axis label
 function renderX(newXScale, xAxis) {
@@ -47,176 +45,191 @@ function renderX(newXScale, xAxis) {
       .call(bottomAxis);
   
     return xAxis;
-  }
-
+}
 
 // function used for updating circles group with a transition to new circles
 function renderCircles(circlesGroup, newXScale, chosenXAxis) {
 
-    circlesGroup.transition()
-      .duration(1000)
-      .attr("cx", d => newXScale(d[chosenXAxis]));
-  
-    return circlesGroup;
-  }
-  
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
 // function used for updating circles group with new tooltip
 function updateToolTip(chosenXAxis, circlesGroup) {
 
-    var label;
-  
-    if (chosenXAxis === "average_income") {
-      label = "Avg Income";
-    }
-    else if (chosenXAxis === "median_income") {
-      label = "Med Income";
-    }
-    else if (chosenXAxis === "total_average_crime_rate") {
-        label = "Crime Rate";
-    }
-  
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`${d.neighbourhood}<br>${label}: ${d[chosenXAxis]}`);
-      });
-  
-    circlesGroup.call(toolTip);
-  
-    circlesGroup.on("mouseover", function(data) {
-      toolTip.show(data);
-    })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
-  
-    return circlesGroup;
+  var label;
+
+  if (chosenXAxis === "average_income") {
+    label = "Avg Income";
   }
-  
-// Import Data
+  else if (chosenXAxis === "median_income") {
+    label = "Med Income";
+  }
+  else if (chosenXAxis === "total_average_crime_rate") {
+      label = "Crime Rate";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.neighbourhood}<br>${label}: ${d[chosenXAxis]}<br>restaurants: ${d.number_of_restaurants}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
 var url = '/api/scatterplotdata'
-d3.json(url).then(function(data) {
-    console.log(data)
+d3.json(url).then(function(scatterData) { 
+    
+    // data.forEach(function (data) {
+    //   data.average_income = +data.average_income
+    // });
 
-  // xLinearScale function above json import
-  var xLinearScale = xScale(data, chosenXAxis);
+      // parse data
+    scatterData.forEach(function(data) {
+        data.average_income = +data.average_income;
+        data.median_income = +data.median_income;
+        data.number_of_restaurants = +data.number_of_restaurants;
+        data.total_average_crime_rate = +data.total_average_crime_rate;
+    });
 
-  // Create y scale function
-  var yLinearScale = d3.scaleLinear()
-                .domain([0, d3.max(data, d => d.number_of_restaurant)])
-                .range([height, 0]);
+    console.log(d3.min(scatterData, d => d[chosenXAxis]))
+    console.log(d3.max(scatterData, d => d[chosenXAxis]))
+    console.log(d3.max(scatterData, d => d.number_of_restaurants))
 
-  // Create initial axis functions
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+    // xLinearScale function above json import
+    var xLinearScaleSc = xScale(scatterData, chosenXAxis);
 
-  // append x axis
-  var xAxis = chartGroup.append("g")
-      .classed("x-axis", true)
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
+    // Create y scale function
+    var yLinearScaleSc = d3.scaleLinear()
+        .domain([0, d3.max(scatterData, d => d.number_of_restaurants)])
+        .range([heightScatter, 0]);
 
-  // append y axis
-  chartGroup.append("g")
-            .call(leftAxis);
 
-  // append initial circles
-  var circlesGroup = chartGroup.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xLinearScale(d[chosenXAxis]))
-        .attr("cy", d => yLinearScale(d.number_of_restaurant))
-        .attr("r", 20)
-        .attr("fill", "pink")
-        .attr("opacity", ".5");
 
-  // Create group for three x-axis labels
-  var labelsGroup = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+    // Create initial axis functions
+    var bottomAxisSc = d3.axisBottom(xLinearScaleSc);
+    var leftAxisSc = d3.axisLeft(yLinearScaleSc);
+ 
+    // append x axis
+    var xAxisSc = chartGroupScatter.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${heightScatter})`)
+        .call(bottomAxisSc);
 
-  var avgIncomeLabel = labelsGroup
+    // append y axis
+    chartGroupScatter.append("g")
+                .call(leftAxisSc);
+
+    // append initial circles
+    var circlesGroup = chartGroupScatter.selectAll("circle")
+            .data(scatterData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScaleSc(d[chosenXAxis]))
+            .attr("cy", d => yLinearScaleSc(d.number_of_restaurants))
+            .attr("r", 10)
+            .attr("fill", "#82b8ad")
+            .attr("opacity", ".7");
+
+    // Create group for three x-axis labels
+    var labelsGroup = chartGroupScatter.append("g")
+    .attr("transform", `translate(${widthScatter / 2}, ${heightScatter + 20})`);
+
+    var avgIncomeLabel = labelsGroup
       .append("text")
       .attr("x", 0)
-      .attr("y", 20)
+      .attr("y", 15)
       .attr("value", "average_income") // value to grab for event listener
       .classed("active", true)
       .text("Average Income");
 
-  var medIncomeLabel = labelsGroup
+    var medIncomeLabel = labelsGroup
       .append("text")
       .attr("x", 0)
-      .attr("y", 40)
+      .attr("y", 35)
       .attr("value", "median_income") // value to grab for event listener
       .classed("inactive", true)
       .text("Median Income");
 
-  var avgCrimeRate = labelsGroup
+    var avgCrimeRate = labelsGroup
     .append("text")
     .attr("x", 0)
-    .attr("y", 60)
+    .attr("y", 55)
     .attr("value", "total_average_crime_rate") // value to grab for event listener
     .classed("inactive", true)
     .text("Average Crime Rate")
 
-  // append y axis
-  chartGroup.append("text")
+    // append y axis
+    chartGroupScatter.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
+        .attr("y", 0 - marginScatter.left)
+        .attr("x", 0 - (heightScatter / 2))
         .attr("dy", "1em")
         .classed("active", true)
         .text("Number of Restaurants");
 
-  // updateToolTip function above json import
-  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+    // updateToolTip function above json import
+    // var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+    // x axis labels event listener
+    labelsGroup.selectAll("text").on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      if (value !== chosenXAxis) {
 
-// x axis labels event listener
-labelsGroup.selectAll("text").on("click", function() {
-    // get value of selection
-    var value = d3.select(this).attr("value");
-    if (value !== chosenXAxis) {
+        // replaces chosenXAxis with value
+        chosenXAxis = value;
 
-      // replaces chosenXAxis with value
-      chosenXAxis = value;
+        // functions here found above json import
+        // updates x scale for new data
+        xLinearScaleSc = xScale(scatterData, chosenXAxis);
 
-      // console.log(chosenXAxis)
+        // updates x axis with transition
+        xAxisSc = renderX(xLinearScaleSc, xAxisSc);
 
-      // functions here found above json import
+        // updates circles with new x values
+        circlesGroup = renderCircles(circlesGroup, xLinearScaleSc, chosenXAxis);
 
-      // updates x scale for new data
-      xLinearScale = xScale(data, chosenXAxis);
+        // updates tooltips with new info
+        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
 
-      // updates x axis with transition
-      xAxis = renderX(xLinearScale, xAxis);
-
-      // updates circles with new x values
-      circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-      // updates tooltips with new info
-      circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-      // changes classes to change bold text
-      if (chosenXAxis === "average_income") {
-        avgIncomeLabel.classed("active", true).classed("inactive", false);
-        medIncomeLabel.classed("active", false).classed("inactive", true);
-        avgCrimeRate.classed("active", false).classed("inactive", true);
+        // changes classes to change bold text
+        if (chosenXAxis === "average_income") {
+          avgIncomeLabel.classed("active", true).classed("inactive", false);
+          medIncomeLabel.classed("active", false).classed("inactive", true);
+          avgCrimeRate.classed("active", false).classed("inactive", true);
+        }
+        else if (chosenXAxis === "median_income") {
+          avgIncomeLabel.classed("active", false).classed("inactive", true);
+          medIncomeLabel.classed("active", true).classed("inactive", false);
+          avgCrimeRate.classed("active", false).classed("inactive", true);
+        }
+        else if (chosenXAxis === "total_average_crime_rate") {
+          avgIncomeLabel.classed("active", false).classed("inactive", true);
+          medIncomeLabel.classed("active", false).classed("inactive", true);
+          avgCrimeRate.classed("active", true).classed("inactive", false);
+        }
       }
-      else if (chosenXAxis === "median_income") {
-        avgIncomeLabel.classed("active", false).classed("inactive", true);
-        medIncomeLabel.classed("active", true).classed("inactive", false);
-        avgCrimeRate.classed("active", false).classed("inactive", true);
-      }
-      else if (chosenXAxis === "total_average_crime_rate") {
-        avgIncomeLabel.classed("active", false).classed("inactive", true);
-        medIncomeLabel.classed("active", false).classed("inactive", true);
-        avgCrimeRate.classed("active", true).classed("inactive", false);
-      }
-    }
-  });
-  }).catch(function(error) {
-  console.log(error);
-    
+    });
+
+}).catch(function(error) {
+console.log(error);
+ 
 });
+
+    
+
